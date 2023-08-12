df4a79e7fdf94620fc69c3aa6df0b0f65ab01d45
Ross Fenning <github@rossfenning.co.uk>
2023-08-12 17:26:56 +0100

    Add publish conf for live version

---
+++ publishconf.py
@@ -0,0 +1,10 @@
+from pelicanconf import *  # noqa
+
+SITEURL = "http://music.avengerpenguin.com"
+RELATIVE_URLS = False
+
+DELETE_OUTPUT_DIRECTORY = False
+
+# GOOGLE_TAG_ID = ""
+# GOOGLE_ADSENSE_ID = ""
+# DISQUS_SITE = ""
